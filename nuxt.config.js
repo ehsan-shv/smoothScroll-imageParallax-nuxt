847becd0269d5c8971cef935c087296006fcfd48
eh. sh <sh.ehsan74.es@gmail.com>
2021-03-13 11:06:06 +0330

    github page deploy fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
-  target: 'static', // default is 'server'
+  target: 'static', // default is 'server',
+  router: {
+    base: '/smoothScroll-imageParallax-Nuxt/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'smothScroll-imageParallax-Nuxt',
