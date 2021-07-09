e5cd44591dfca547a25a8d9654cfd2ba9ebda23f
eh. sh <sh.ehsan74.es@gmail.com>
2021-07-09 12:01:31 +0430

    96cf907

---
+++ _nuxt/static/1625815884/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FsmoothScroll-imageParallax-nuxt\u002F_nuxt\u002Fstatic\u002F1625815884",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002FsmoothScroll-imageParallax-nuxt\u002F",assetsPath:"\u002FsmoothScroll-imageParallax-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
